6289f5962968807eeb4557eb94e921036a9b583f
LinkNexus <deitykings@gmail.com>
2024-11-08 11:20:19 +0100

    Adjusted the calculator to use the built-in Function function of JavaScript.

--- script.js
+++ script.js
@@ -2,22 +2,14 @@ const resultScreen = document.querySelector('.result'),
     operationScreen = document.querySelector('.operation'),
     deleteButton = document.querySelector('.delete'),
     clearButton = document.querySelector('.clear'),
-    multiplyButton = document.querySelector('.multiplication'),
-    divisionButton = document.querySelector('.division'),
-    additionButton = document.querySelector('.addition'),
-    subtractionButton = document.querySelector('.subtraction'),
     inputButtons = document.querySelectorAll('.input'),
     plusOrMinusButton = document.querySelector('.plus-or-minus'),
     squareRootButton = document.querySelector('.square-root'),
     equalToButton = document.querySelector('.equal-to');
 
-let firstNumber,
-    secondNumber;
-
 for (const inputButton of inputButtons) {
     inputButton.addEventListener('click', function () {
         resultScreen.value += inputButton.innerText;
-        console.log(1);
     })
 }
 
@@ -45,102 +37,24 @@ function squareRoot() {
 
 squareRootButton.addEventListener('click', squareRoot);
 
-// Addition Function
-
-function addition() {
-    if (resultScreen.value !== '') {
-        firstNumber = parseFloat(resultScreen.value);
-    } else {
+function calculate() {
+    if (resultScreen.value === '') {
         return;
     }
 
-    resultScreen.value = '';
+    const operation = resultScreen.value.replace('×', '*');
+    try {
+        const result = Function(`return ${operation}`)();
 
-    equalToButton.addEventListener('click', () => {
-        secondNumber = parseFloat(resultScreen.value);
-
-        if (!isNaN(firstNumber + secondNumber)) {
-            resultScreen.value = firstNumber + secondNumber;
-        } else {
-            resultScreen.value = 'Syntax Error';
+        if (result === Infinity || isNaN(result) || result === -Infinity) {
+            resultScreen.value = 'Math Error';
+            return;
         }
-    })
-}
 
-additionButton.addEventListener('click', addition);
-
-// Subtraction Function
-
-function subtraction() {
-    if (resultScreen.value !== '') {
-        firstNumber = parseFloat(resultScreen.value);
-    } else {
-        return;
+        resultScreen.value = result;
+    } catch (error) {
+        resultScreen.value = 'Syntax Error';
     }
-
-    resultScreen.value = '';
-
-    equalToButton.addEventListener('click', () => {
-        secondNumber = parseFloat(resultScreen.value);
-
-        if (!isNaN(firstNumber - secondNumber)) {
-            resultScreen.value = firstNumber - secondNumber;
-        } else {
-            resultScreen.value = 'Syntax Error';
-        }
-    })
 }
 
-subtractionButton.addEventListener('click', subtraction);
-
-// Multiplication Function
-
-function multiplication() {
-    if (resultScreen.value !== '') {
-        firstNumber = parseFloat(resultScreen.value);
-    } else {
-        return;
-    }
-
-    resultScreen.value = '';
-
-    equalToButton.addEventListener('click', () => {
-        secondNumber = parseFloat(resultScreen.value);
-
-        if (!isNaN(firstNumber * secondNumber)) {
-            resultScreen.value = firstNumber * secondNumber;
-        } else {
-            resultScreen.value = 'Syntax Error';
-        }
-    })
-}
-
-multiplyButton.addEventListener('click', multiplication);
-
-// Division Function
-
-function division() {
-    if (resultScreen.value !== '') {
-        firstNumber = parseFloat(resultScreen.value);
-    } else {
-        return;
-    }
-
-    resultScreen.value = '';
-
-    equalToButton.addEventListener('click', () => {
-        secondNumber = parseFloat(resultScreen.value);
-
-        if (!isNaN(firstNumber / secondNumber)) {
-            resultScreen.value = firstNumber / secondNumber;
-        } else {
-            resultScreen.value = 'Syntax Error';
-        }
-    })
-}
-
-divisionButton.addEventListener('click', division);
-
-console.log(parseFloat(''));
-
-
+equalToButton.addEventListener('click', calculate);
